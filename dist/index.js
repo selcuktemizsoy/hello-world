2212028f94b8d1317458e993b7f4858c71ad6930
selcuktemizsoy <selcuktemizsoy@gmail.com>
2022-10-12 09:25:25 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
+var hello_world_1 = require("./hello-world");
+Object.defineProperty(exports, "helloWorld", { enumerable: true, get: function () { return hello_world_1.helloWorld; } });
+//# sourceMappingURL=index.js.map
